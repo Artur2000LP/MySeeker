fa38acc3a5cf78a58c725ac62d411e2cb66313ca
Artur <113075481+Artur2000LP@users.noreply.github.com>
2023-11-18 20:49:43 -0500

    Add files via upload

---
+++ suggestions.js
@@ -0,0 +1,23 @@
+let suggestions = [
+	'Youtube',
+	'Desarrollador Web',
+	'Diseñador Web',
+	'Formularios con HTML y CSS',
+	'Crear un canal en Youtube',
+	'Dinero en la programación',
+	'Programación en Python',
+	'Desarrollo Frontend',
+	'Desarrollo Backend',
+	'Rest api con Nodejs',
+	'Como crear un blog con Django',
+	'Como crear un sitio web responsive',
+	'Como consumir una api con React',
+	'Aprender React desde cero',
+	'Aprender Css desde cero',
+	'Aprender Html desde cero',
+	'Como convertirse en programador rapidamente',
+	'Aprender python desde cero',
+	'como desarrollar un sistema de ventas',
+	'como hacer un buscador',
+	'pelicula completa en español latino'
+];
